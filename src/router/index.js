717d9c8239cd87b9481afd2d1246cf1968d78503
Vinicius Emidio Bosi <viniemidiobosi@gmail.com>
2022-03-13 14:27:30 -0300

    fix identification

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: function () {
-      return import('@/views/HomeView.vue')
-    }
-  },
-  {
-    path: '/settings',
-    name: 'settings',
-    component: function () {
-      return import('@/views/SettingsView.vue')
-    }
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: function () {
-      return import('@/views/AboutView.vue')
-    }
-  }
+	{
+		path: '/',
+		name: 'home',
+		component: function () {
+			return import('@/views/HomeView.vue')
+		}
+	},
+	{
+		path: '/settings',
+		name: 'settings',
+		component: function () {
+			return import('@/views/SettingsView.vue')
+		}
+	},
+	{
+		path: '/about',
+		name: 'about',
+		component: function () {
+			return import('@/views/AboutView.vue')
+		}
+	}
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
 export default router
